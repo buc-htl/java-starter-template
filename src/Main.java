8703166635eeeb7f1895ed5bd757a89d6dd0a7e2
buc <buc@htl.rennweg.at>
2025-03-07 21:50:18 +0100

    test commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Projekt erfolgreich gestartet!");
+        System.out.println("Projekt erfolgreich gestartet!!");
         System.out.println("Du kannst diese Datei löschen und mit deinen Übungen beginnen.");
     }
 }
